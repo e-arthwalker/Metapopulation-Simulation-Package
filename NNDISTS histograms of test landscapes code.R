a561dfde67faa4b3d208a7ffc8f061ae37092bc8
e-arthwalker <38920686+e-arthwalker@users.noreply.github.com>
2018-11-23 14:45:40 -0500

    Add files via upload

--- NNDISTS histograms of test landscapes code.R
+++ NNDISTS histograms of test landscapes code.R
@@ -5,7 +5,8 @@
 ############################################################################################################
 ############################################################################################################
 directory="C:/Users/abuga/Desktop/Metapopulation Manuscript Output"
-landscapes.data<-read.csv("50_p_10landscapes_clevel500.csv")
+landscapes.data<-read.csv("50_p_100landscapes_clevel500.csv")
+no.replicates=100
 ############################################################################################################
 ############################################################################################################
 
@@ -25,19 +26,19 @@ regular.data<-subset(landscapes.data, landscape.type=="regular")
 
 #CREATING THE HISTOGRAMS (also gives skew, kurtosis and the mean minimum nearest neighbour intepatch distance)
 ##############################################################################################################
-plot.nndist<-function(data, title){
-  avg.data<-data.frame(seq(1:10), rep(NA, 10))
+plot.nndist<-function(data, title, no.replicates){
+  avg.data<-data.frame(seq(1:no.replicates), rep(NA, no.replicates))
   colnames(avg.data)<-c("landscape", "avg.nndist")
-  min.data<-data.frame(seq(1:10), rep(NA, 10))
+  min.data<-data.frame(seq(1:no.replicates), rep(NA, no.replicates))
   colnames(min.data)<-c("landscape", "min.nndist")
   for (i in 1:nrow(avg.data)){
-    avg.data[i,2]<-mean(nndist(data[(50*i-49):(50*i),6:55]))
-    min.data[i,2]<-min(nndist(data[(50*i-49):(50*i),6:55]))   
+    avg.data[i,2]<-mean(nndist(data[(50*i-49):(50*i),6:55])) #gives mean nndist for each landscape
+    min.data[i,2]<-min(nndist(data[(50*i-49):(50*i),6:55]))   #gives minimum nndist for each landscape
   }
-  hist(avg.data$avg.nndist, xlab="Nearest Neighbour Interpatch Distances", ylab="Avg. Frequency within Landscapes", 
-       main = title, xlim=range(1:10))
-  hist(min.data$min.nndist, xlab="Nearest Neighbour Interpatch Distances", ylab="Avg. Frequency within Landscapes", 
-       main = title, xlim=range(1:10))
+  hist(avg.data$avg.nndist, xlab="Nearest Neighbour Interpatch Distances", ylab="Avg. Frequency Avg. NNDist within Landscapes", 
+       main = title, xlim=range(1:10)) #historgram of avg nndists within landscapes
+  hist(min.data$min.nndist, xlab="Nearest Neighbour Interpatch Distances", ylab="Avg. Frequency Min NNDIST within Landscapes", 
+       main = title, xlim=range(1:10)) #hist of minimum nndists within landscapes
   avg.min.nndist<-mean(min.data$min.nndist)
   skewness<-skewness(avg.data$avg.nndist)
   kurtosis<-kurtosis(avg.data$avg.nndist)
@@ -45,6 +46,6 @@ plot.nndist<-function(data, title){
 }
 ###############################################################################################################
 #CHECK/GET OUTPUT
-plot.nndist(regular.data, "More Uniform Landscapes")
-plot.nndist(random.data, "Random Landscapes")
-plot.nndist(clustered.data, "More Clustered Landscapes")
+plot.nndist(regular.data, "More Uniform Landscapes", no.replicates)
+plot.nndist(random.data, "Random Landscapes", no.replicates)
+plot.nndist(clustered.data, "More Clustered Landscapes", no.replicates)
--- P and LM and SimEq in parallel 3 (50p).R
+++ P and LM and SimEq in parallel 3 (50p).R
@@ -4,8 +4,8 @@ setwd("C:/Users/Administrator/Desktop/Metapopulation-Simulation-Package-master")
 source("Create Landscape Function.r") #load create.landscape.function
 source("Lambda M Function.r") #load the calculate lamda.M.function
 source("Pstar Function.r") #load the persistence.function
-source("time to eq.r") #load the SRLM.sim function
-source("Destroy and Degrade a Landscape Function2 (P and LM and SimEq).r") #destroy.vs.degrade.function only calculating Pstar and Lambda.M 
+source("time to eq3.r") #load the SRLM.sim function
+source("Destroy and Degrade a Landscape Function2 (P and LM and SimEq)2.r") #destroy.vs.degrade.function only calculating Pstar and Lambda.M 
 source("Degrade and Destroy Multiple Landscapes Function.r") #replicates landscape creation and destroy vs. degrade specified metapop parameters a specified number of times
 source("Vary Alpha Destroy and Degrade Function for parallel.r") #provides replicates for a range of alphas across a landscape type
 
@@ -22,7 +22,7 @@ registerDoParallel(cl)
 #parallel alphas
 ##############################################################################
 parallel.alphas<-function(j, n.reps, k){
-  alpha.data<-read.csv("C:/Users/Administrator/Desktop/Manuscript/Data and Figures/50 patches/alphas2.csv")
+  alpha.data<-read.csv("C:/Users/Administrator/Desktop/Metapopulation-Simulation-Package-master/alphas.csv")
   #alpha.data<-alpha.data[,-1]
   alpha.data<-alpha.data[,-10]
   regular.data<-vary.alpha.in.parallel(alphas=alpha.data[1,j+1], landscape.type="regular", n.landscapes=n.reps, k=k, n.patches=50, landscape.limit=100, no.runs=500, scaled.lm=20)
@@ -31,11 +31,11 @@ parallel.alphas<-function(j, n.reps, k){
   rbind(regular.data, random.data, clustered.data)
 }
 
-its.of.5=100
+its.of.5=10
 output<- foreach (k=1:5, .combine=rbind)  %:%
  foreach(i=((k*8)-7):(k*8), .combine=rbind) %dopar% parallel.alphas(j=i-((k-1)*8), n.reps=its.of.5, k=k)
-write.csv(output, "500reps_PnLMnSimEq_3_50p.csv")
-#didn't work for 100 but worked for 20
+write.csv(output, "50reps_PnLMnSimEq_3_50pnew.csv")
+
 
 # turn parallel processing off and run sequentially again:
 registerDoSEQ()
